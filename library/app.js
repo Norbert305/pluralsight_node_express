40d7c597ebb4e431ee71738b636e334e6e1f1f10
Norbert305 <sealsnorbert@yahoo.com>
2023-05-01 19:04:27 -0400

    new stuff added

--- library/app.js
+++ library/app.js
@@ -12,10 +12,15 @@ app.get("/", function(req, res,){
     res.send("Hello from my library app");
 });
 
+//connect your html on the browser
 app.get("/frontend", function(req,res){
     res.sendFile(path.join(__dirname,'views/index.html'));
 })
 
+//need to connect your css and javascript in the public folder to the browser
+app.use(express.static(path.join(path.dirname(__dirname, '/public/'))))
+
+//connect your server
 app.listen(3000, function() {
     console.log("Server has started on port....." + chalk.green("3000"));
 });

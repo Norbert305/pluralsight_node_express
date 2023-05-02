969e5d55b19c46c48dc54cb5ea7c9f4362bdad19
Norbert305 <sealsnorbert@yahoo.com>
2023-05-02 16:43:20 -0400

    new stuff added day 2

--- library/app.js
+++ library/app.js
@@ -6,6 +6,10 @@ var chalk = require("chalk");
 
 var path = require("path");
 
+const dotenv = require('dotenv');
+
+const port = process.env.PORT || 3000 
+
 const mongoose = require('mongoose');//import mongoose
 
 app.get("/", function(req, res,){
@@ -13,14 +17,23 @@ app.get("/", function(req, res,){
 });
 
 //connect your html on the browser
-app.get("/frontend", function(req,res){
+app.get("/home", function(req,res){
     res.sendFile(path.join(__dirname,'views/index.html'));
+})
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,'views/index2.html'));
+})
+app.get("/services", function(req,res){
+    res.sendFile(path.join(__dirname,'views/index3.html'));
+})
+app.get("/contact", function(req,res){
+    res.sendFile(path.join(__dirname,'views/index4.html'));
 })
 
 //need to connect your css and javascript in the public folder to the browser
 app.use(express.static(path.join(path.dirname(__dirname, '/public/'))))
 
 //connect your server
-app.listen(3000, function() {
-    console.log("Server has started on port....." + chalk.green("3000"));
+app.listen(port, function() {
+    console.log(`Server has started on port...${chalk.green(port)}`);
 });
